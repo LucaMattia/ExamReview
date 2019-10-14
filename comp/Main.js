daa69ca770fc35111c25dba00bb40eaeeb7d0242
patrick4web <pat-wong@hotmail.com>
2019-10-14 13:55:29 -0700

    Something wrong color not changing

--- comp/Main.js
+++ comp/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import Mid from '../comp/Mid';
+
 
 function Main(){
     return(
         <View>
+
+            <Mid />
         </View>
     )
 }
